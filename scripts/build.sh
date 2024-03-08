e432347b1780d78c0bd0a01f13a8b1299d679494
Jia <49358172+craftslab@users.noreply.github.com>
2024-03-08 03:38:42 +0000

    feat(release): upgrade to bazel-buildfarm v2.9.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # buildfarm-server: bazel-bin/src/main/java/build/buildfarm/buildfarm-server_deploy.jar
-bazel build //src/main/java/build/buildfarm:buildfarm-server_deploy.jar
+bazel-6.4.0 build //src/main/java/build/buildfarm:buildfarm-server_deploy.jar
 
 # buildfarm-shard-worker: bazel-bin/src/main/java/build/buildfarm/buildfarm-shard-worker_deploy.jar
-bazel build //src/main/java/build/buildfarm:buildfarm-shard-worker_deploy.jar
+bazel-6.4.0 build //src/main/java/build/buildfarm:buildfarm-shard-worker_deploy.jar
 
 # bf-cat: bazel-bin/src/main/java/build/buildfarm/tools/bf-cat_deploy.jar
-bazel build //src/main/java/build/buildfarm/tools:bf-cat_deploy.jar
+bazel-6.4.0 build //src/main/java/build/buildfarm/tools:bf-cat_deploy.jar
 
 # bf-find-operations: bazel-bin/src/main/java/build/buildfarm/bf-find-operations_deploy.jar
-bazel build //src/main/java/build/buildfarm:bf-find-operations_deploy.jar
+bazel-6.4.0 build //src/main/java/build/buildfarm:bf-find-operations_deploy.jar
