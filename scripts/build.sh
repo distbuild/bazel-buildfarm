9cdb88c14b794cd8cc4c842c2864210f91cd120b
Jia <49358172+craftslab@users.noreply.github.com>
2024-03-08 10:58:26 +0800

    feat(scripts): add support for building bf-find-operations

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,3 +8,6 @@ bazel build //src/main/java/build/buildfarm:buildfarm-shard-worker_deploy.jar
 
 # bf-cat: bazel-bin/src/main/java/build/buildfarm/tools/bf-cat_deploy.jar
 bazel build //src/main/java/build/buildfarm/tools:bf-cat_deploy.jar
+
+# bf-find-operations: bazel-bin/src/main/java/build/buildfarm/bf-find-operations_deploy.jar
+bazel build //src/main/java/build/buildfarm:bf-find-operations_deploy.jar
