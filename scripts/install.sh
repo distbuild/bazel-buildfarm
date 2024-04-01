2ad5eed3da82509669663c5dcac379b796e986b5
Jia <49358172+craftslab@users.noreply.github.com>
2024-04-01 15:48:34 +0800

    feat(scripts): install openjdk-17 for gh actions

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,7 @@ sudo apt install apt-transport-https curl gnupg -y
 curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -o bazelisk
 chmod +x bazelisk
 sudo mv bazelisk /usr/local/bin
+
+# Install JDK
+sudo apt update -y
+sudo apt install -y openjdk-17-jdk
